eaa8443d0628b9025300ad38ec0fdc08411161e9
Awoo <fefa12@yandex.ru>
2025-05-08 20:41:12 +0300

    Пропущенные изменения файлы FlighTicket, Passanger Json файлы из которых берем историю.

--- src/main/java/ru/easet/aviacassa/model/FlightTicket.java
+++ src/main/java/ru/easet/aviacassa/model/FlightTicket.java
@@ -1,20 +1,32 @@
 package ru.easet.aviacassa.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 
 /**
  * Конкретная реализация билета.
  */
 public class FlightTicket extends Ticket {
+
     /**
-     * @param passenger     пассажир
-     * @param flight        рейс
-     * @param ticketClass   класс билета
-     * @param price         стоимость
-     * @param purchaseDate  дата покупки
+     * Jackson‐конструктор для десериализации из JSON.
+     *
+     * @param passenger    пассажир
+     * @param flight       рейс
+     * @param ticketClass  класс билета
+     * @param price        стоимость
+     * @param purchaseDate дата покупки
      */
-    public FlightTicket(Passenger passenger, Flight flight, String ticketClass,
-                        double price, LocalDate purchaseDate) {
+    @JsonCreator
+    public FlightTicket(
+            @JsonProperty("passenger") Passenger passenger,
+            @JsonProperty("flight") Flight flight,
+            @JsonProperty("ticketClass") String ticketClass,
+            @JsonProperty("price") double price,
+            @JsonProperty("purchaseDate") LocalDate purchaseDate
+    ) {
         super(passenger, flight, ticketClass, price, purchaseDate);
     }
 }
--- src/main/java/ru/easet/aviacassa/model/Passenger.java
+++ src/main/java/ru/easet/aviacassa/model/Passenger.java
@@ -10,6 +10,9 @@ public class Passenger {
     private LocalDate birthDate;
     private String passportNumber;
 
+    /** Jackson: default‐конструктор */
+    public Passenger() {}
+
     public Passenger(String name, LocalDate birthDate, String passportNumber) {
         this.name = name;
         this.birthDate = birthDate;
@@ -19,4 +22,18 @@ public class Passenger {
     public String getName() { return name; }
     public LocalDate getBirthDate() { return birthDate; }
     public String getPassportNumber() { return passportNumber; }
+
+
+    // ========== Сеттеры (для Jackson) ==========
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
 }
