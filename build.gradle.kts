3a4f1558f80a45ff892d9cbe49d35ea7c095220f
Awoo <fefa12@yandex.ru>
2025-05-08 12:52:52 +0300

    Gradle:     // Основная библиотека Jackson     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")     // Модуль для java.time     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    // Основная библиотека Jackson
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    // Модуль для java.time
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
